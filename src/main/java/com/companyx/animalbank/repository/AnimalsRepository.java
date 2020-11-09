4d0e0ba290db9e126df9e88b855d98b9183b3ead
Keith <keithomiping@gmail.com>
2020-11-09 23:30:47 +0800

    Add unit test for AnimalService

--- src/main/java/com/companyx/animalbank/repository/AnimalsRepository.java
+++ src/main/java/com/companyx/animalbank/repository/AnimalsRepository.java
@@ -1,7 +1,6 @@
 package com.companyx.animalbank.repository;
 
 import com.companyx.animalbank.entity.Animal;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
